8761aae2c920bc8a2dde2851d776d02a267cefaf
ajaymahar <ajay.mhar@gmail.com>
2020-11-15 19:38:01 +0530

    initial commit for GCE demos

---
+++ GCE/setup-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setting up new environment for GCE demos
+echo "Creatre new custom VPC network"
+
+#Create subnet in us-east1 region
+echo "Creating private subnet in region us-east1"
+
+
+#Create subnet in us-east4 region
+echo "create private subnet on us-east4 region"
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Google-Cloud-Demo
 This repo will be single source of all the demos for GCP.
+
+instruction to use demo scripts comming soon....
